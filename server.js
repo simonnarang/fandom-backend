11b6debb119e7a01b8314b9c549dd187292c1898
Simon Narang <simonarang202@gmail.com>
2015-08-20 14:37:17 -0400

    Port Change

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post('/login',function(req,res){
   console.log("User name = "+user_name+", password is "+password);
   res.end("yes");
 });
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 1738);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 http.createServer(app).listen(app.get('port') ,app.get('ip'), function (req, res) {
   console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
